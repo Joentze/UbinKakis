5313f43692ddd40754ab40bc7460ec51803cfce7
Joen Tan <joentze01@gmail.com>
2025-02-26 16:46:42 +0800

    feat: added team card fixed width

--- frontend/src/app/components/card/TeamCard.tsx
+++ frontend/src/app/components/card/TeamCard.tsx
@@ -1,39 +1,36 @@
-import { Box, Card, Inset, Text } from "@radix-ui/themes"
-
+import { Box, Card, Inset, Text } from "@radix-ui/themes";
 
 interface ITeamCard {
-    team: string
-    image: string
-    slug: string
+    team: string;
+    image: string;
+    slug: string;
 }
 
 const TeamCard: React.FC<ITeamCard> = ({ team, image, slug }) => {
     return (
-        <Box maxWidth={"350px"} className="m-auto animate-fade-in">
-            <Card size="4" className="mx-1" >
+        <Box width={"350px"} className="m-auto animate-fade-in">
+            <Card size="4" className="mx-1">
                 <Inset clip="padding-box" side="top" pb="current">
-                    < img
-
+                    <img
                         src={image}
-                        alt={`img-${team}`
-                        }
+                        alt={`img-${team}`}
                         style={{
-                            display: 'block',
-                            objectFit: 'cover',
-                            width: '100%',
+                            display: "block",
+                            objectFit: "cover",
+                            width: "100%",
                             height: 200,
-                            backgroundColor: 'var(--gray-5)',
+                            backgroundColor: "var(--gray-5)",
                         }}
                     />
-                </Inset >
+                </Inset>
                 <a href={`/t/${slug}`}>
                     <h1 className="text-center text-3xl text-gray-600 hover:underline cursor-pointer truncate">
                         {team}
                     </h1>
                 </a>
-            </Card >
+            </Card>
         </Box>
-    )
-}
+    );
+};
 
-export default TeamCard
+export default TeamCard;
